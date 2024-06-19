4d20c0e3f5dc75eff15ef5e35c1d8ef6fb2750f2
Pedro <pg20041312@gmail.com>
2024-06-18 23:44:17 -0300

    portoflio

---
+++ src/data/constants.js
@@ -0,0 +1,405 @@
+export const Bio = {
+  name: "Pedro Guilherme",
+  roles: [
+    "Desenvolvedor Full Stack",
+    "Desenvolvedor Mobile",
+  ],
+  description:
+    "Desenvolvedor Full Stack apaixonado por transformar ideias em realidade digital. Com experiência abrangente em HTML, CSS, JavaScript e tecnologias como React e Node.js, tenho um forte foco em criar soluções eficientes e intuitivas. Minha jornada inclui projetos desafiadores que valorizam a inovação e a qualidade. Estou sempre em busca de novos horizontes para contribuir de forma significativa e agregar valor a equipes dinâmicas e projetos ambiciosos.",
+  github: "https://github.com/pedroGuilherme1969",
+  resume:
+    "https://docs.google.com/document/d/1rFWfiRfWdEkOIb2OYEgjgTCnIDYPgZBqx45PnNe8GkU/edit?usp=sharing",
+  linkedin: "www.linkedin.com/in/pedro-guilherme-silva1969",
+  insta: "https://www.instagram.com/_https.pedro__/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "https://www.svgrepo.com/show/521303/react-16.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "https://www.svgrepo.com/show/354113/nextjs-icon.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://www.vectorlogo.zone/logos/javascript/javascript-icon.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-icon.svg",
+      },
+      {
+        name:"Tailwind",
+        image:
+        "https://www.svgrepo.com/show/374118/tailwind.svg"
+      },
+
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/logos/nodejsLight.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg",
+      },
+      {
+        name: "Graph Ql",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name:"C#",
+        image:
+        "https://www.svgrepo.com/show/452184/csharp.svg"
+      },
+      {
+        name:"Ruby",
+        image:
+        "https://www.svgrepo.com/show/452095/ruby.svg"
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://www.svgrepo.com/show/303232/mongodb-logo.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://www.svgrepo.com/show/31053/xml.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/95/Android_Studio_Icon_3.6.svg",
+      },
+    ],
+  },
+  {
+    title: "Outras",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://www.svgrepo.com/show/512317/github-142.svg",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://www.vectorlogo.zone/logos/netlify/netlify-icon.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://www.svgrepo.com/show/354522/visual-studio-code.svg",
+      },
+      {
+        name: "Visual Studio",
+        image:
+        "https://www.svgrepo.com/show/354520/visual-studio.svg"
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://www.vectorlogo.zone/logos/figma/figma-icon.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.svgrepo.com/show/525810/code-circle.svg",
+    role: "Desenvolvedor Web",
+    company: "Free Lance",
+    date: "Março 2023 - Presente",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      " Next Js",
+    ], 
+  },
+  {
+    id: 1,
+    img: "https://www.svgrepo.com/show/525810/code-circle.svg",
+    role: "Desenvolvedor Front End",
+    company: "Free Lance",
+    date: "março 2023 - Presente",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://www.svgrepo.com/show/429961/maintenance-repair-construction.svg",
+    role: "Manutenção de Computadores",
+    company: "Free Lance",
+    date: "Março 2021 - Maio 2022",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://cf-vectorizer-live.s3.amazonaws.com/vectorized/anon/2g0Pmt7mugtUDBj6VuwrADgQ2Eo.svg",
+    school: "Faculdade Impacta Tecnologia",
+    date: "Março 2023 - Presente",
+    grade: "8.71 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Tecnólogo - Análise e Desenvolvimento de Sistemas",
+  },
+  {
+    id: 1,
+    img: "https://cf-vectorizer-live.s3.amazonaws.com/vectorized/anon/2g0PtsJCpDYuulFeICabi19X29x.svg",
+    school: "Senac São Paulo",
+    date: "Março 2021 - Maio 2022",
+    grade: "88.2%",
+    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+    degree: "Curso Técnico - Técnico em Informática",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Trackify",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Docker",
+      "AWS",
+      "DuckDNS",
+      "Eslint",
+      "Husky",
+      "CI/CD",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Trackify",
+    webapp: "https://trackify.duckdns.org",
+  },
+  {
+    id: 0,
+    title: "Podstream",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://podstream.netlify.app/",
+    member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Vexa",
+    date: "Oct 2022 - Present",
+    description:
+      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "NodeMailer",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Project-Management-App",
+    webapp: "https://vexa-app.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Brain Tumor Detection",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
+    image:
+      "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
+    webapp: "https://brain-tumor.netlify.app/",
+    member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Buckoid",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
+    image:
+      "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+    tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Buckoid-Android-App",
+    webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  {
+    id: 10,
+    title: "Job Finding App",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
+    tags: ["React Native", "JavaScript", "Axios"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Job-finder-App",
+    webapp: "https://github.com/rishavchanda/Job-finder-App",
+  },  
+  {
+    id: 7,
+    title: "Quiz App",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
+    image:
+      "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
+    tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Quiz-Earn",
+    webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+  {
+    id: 8,
+    title: "Face Recognition",
+    date: "Jan 2021",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
